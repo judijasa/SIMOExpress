97c4e6491f7e97f7d2877b50313fc3c8614234c0
Juan Diego Jaramillo <33137383+judijasa@users.noreply.github.com>
2022-05-24 14:14:41 -0500

    Add files via upload

---
+++ private/client_config copy.sh
@@ -0,0 +1,24 @@
+####
+# Configuration file to be used in connection.php
+#
+# For security, the msyql user 'joe'
+# has 'read only' privileges
+#
+# WARNING: Comments in this script must have
+# their own line
+#
+# This script must have read only privilege for
+# access class 'other'
+# How to manage file privileges at
+# website: kb.iu.edu/d/abdb
+#
+# Show privileges command: ls -l filename
+# Output sctructure: user -- group -- other
+#
+####
+
+# MySQL
+SERVER="127.0.0.1"
+USER="joe"
+PASSWORD="joe_user_password"
+DATABASE="database_name"
